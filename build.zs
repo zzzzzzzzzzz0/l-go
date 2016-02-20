7a51b5852dcfae899cf927f36948edbc38b3c41b
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-02-20 23:55:11 +0800

    改

--- build.zs
+++ build.zs
@@ -1,2 +1,2 @@
 #!/usr/bin/zhscript --。
-加载build-if-need.zs、-s、../../zhscript-go/zhscript/、-m、l。
+加载build-if-need.zs、-s、../../zhscript-go/zhscript/、-m、l/l。
--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-# -zhscript-o-tree -zhscript-o-path -zhscript-o-lc
-bin/l -zhscript-o-ansi `basename $0 .sh`.zs
+bin/l `basename $0 .sh`.zs "$@"
